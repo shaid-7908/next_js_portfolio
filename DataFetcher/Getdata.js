3d58c2eaf69b38ab4db3fabf8bd4ca8a29dd8f09
Shahid <shahid451998@gmail.com>
2023-08-22 21:39:38 +0530

    i think its done

--- DataFetcher/Getdata.js
+++ DataFetcher/Getdata.js
@@ -1,7 +1,8 @@
 import fs from 'fs'
+import path from 'path'
 import matter from 'gray-matter'
 export function getPostData(slug){
-    const fileContents = fs.readFileSync(`POST/${slug}.md`,'utf-8')
+    const fileContents = fs.readFileSync(path.join('POST',slug + '.md'),'utf-8')
     const matterResult = matter(fileContents)
     return matterResult
 }
